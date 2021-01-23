9b944ae147118c3666218896d1f1d5d95148e904
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-01-23 13:23:23 +0400

    feat: tests

--- scripts/test/init.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable linebreak-style */
-export class TestInit {
-  constructor() {
-    
-  }
-}
---
+++ scripts/test/test.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+
+describe("x", function() {
+  it("Возводит в степень n", function() {
+    assert.equal(x(2), 4);
+  });
+})
