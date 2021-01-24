224dede7f132be2acd92aad80882d7c180fc00bb
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-01-24 17:16:21 +0400

    feat: add audio test

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -1,4 +1,40 @@
 /* eslint-disable linebreak-style */
+import { audioAPI } from "../../main.js";
+describe("Проверка аудио", function() {
+  it("Нота А0 сыграла", function() {
+    assert.equal(audioAPI.playNote("A0"), undefined);
+  });
+  it("Нота А1 сыграла", function() {
+    assert.equal(audioAPI.playNote("A1"), undefined);
+  });
+  it("Нота А2 сыграла", function() {
+    assert.equal(audioAPI.playNote("A2"), undefined);
+  });
+  it("Нота А3 сыграла", function() {
+    assert.equal(audioAPI.playNote("A3"), undefined);
+  });
+  it("Нота А4 сыграла", function() {
+    assert.equal(audioAPI.playNote("A4"), undefined);
+  });
+  it("Нота А5 сыграла", function() {
+    assert.equal(audioAPI.playNote("A5"), undefined);
+  });
+  it("Нота А6 сыграла", function() {
+    assert.equal(audioAPI.playNote("A6"), undefined);
+  });
+  it("Нота А7 сыграла", function() {
+    assert.equal(audioAPI.playNote("A7"), undefined);
+  });
+  it("Нота А8 сыграла", function() {
+    assert.equal(audioAPI.playNote("A8"), undefined);
+  });
+})
+
+
+
+
+
+
 describe("Размеры хидера", function() {
   it("Ширина хидера не искажается", function() {
     assert.equal(headerWidth(), document.documentElement.scrollWidth);
