3f140ed252c7f2b6208d84e512a48b712392120e
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-01-24 16:33:23 +0400

    feat: add size tests

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -1,7 +1,27 @@
 /* eslint-disable linebreak-style */
+describe("Размеры хидера", function() {
+  it("Ширина хидера не искажается", function() {
+    assert.equal(headerWidth(), document.documentElement.scrollWidth);
+  });
+  it("Высота хидера не искажается", function() {
+    assert.equal(headerHeight(), 50);
+  });
+})
 
-describe("x", function() {
-  it("Возводит в степень n", function() {
-    assert.equal(x(2), 4);
+describe("Размеры главного блока", function() {
+  it("Ширина главного блока не искажается", function() {
+    assert.equal(mainWidth(), document.documentElement.scrollWidth);
+  });
+  it("Высота главного блока не искажается", function() {
+    assert.equal(mainHeight(), document.documentElement.clientHeight - 120);
+  });
+})
+
+describe("Размеры футера", function() {
+  it("Ширина футера не искажается", function() {
+    assert.equal(footerWidth(), document.documentElement.scrollWidth);
+  });
+  it("Высота футера не искажается", function() {
+    assert.equal(footerHeight(), 70);
   });
 })
