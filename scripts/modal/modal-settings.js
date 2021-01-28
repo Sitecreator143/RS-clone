2567aff80b09b205c5f266723f73496d303f9fc1
Alexander Nikolaev <nikolaevsailing@mail.ru>
2021-01-28 07:52:31 +0400

    refactor: delete repeated code

--- scripts/modal/modal-settings.js
+++ scripts/modal/modal-settings.js
@@ -3,22 +3,33 @@ import { modalTypesObject } from "../../main.js";
 import { Modal } from "./modal.js";
 import { ModalMain } from "./modal-main.js";
 export class ModalSettings extends Modal {
+  findSetting(settingName) {
+    const setting = this.localSettings.playerSettings.find(el => el.settingName === settingName)
+      .settingValue;
+    return setting;
+  }
+
+  createTextBtn() {
+    const textBtn = document.createElement("div");
+    textBtn.classList.add("modal__text-item");
+    this.wrap.appendChild(textBtn);
+    return textBtn;
+  }
+
   createModalSettings() {
     this.clearModalArea();
     this.getWords("settingModal");
     this.createLogo();
     this.createWrap();
 
-    const wrapPlayers = document.createElement("div");
-    let playersCount = this.localSettings.playerSettings.find(el => el.settingName === "playersCount").settingValue;
+    const wrapPlayers = this.createTextBtn();
+    let playersCount = this.findSetting("playersCount");
     wrapPlayers.innerText = `${this.wordsArr[2]} ${playersCount}`;
-    wrapPlayers.classList.add("modal__text-item");
-    this.wrap.appendChild(wrapPlayers);
 
     const wrapNames = document.createElement("div");
     wrapNames.classList.add("modal__wrap-names");
     this.wrap.appendChild(wrapNames);
-    const playersNames = this.localSettings.playerSettings.find(el => el.settingName === "playersNames").settingValue;
+    const playersNames = this.findSetting("playersNames");
     const createAreaForNames = () => {
       wrapNames.innerHTML = "";
       const namesInput = [];
@@ -44,10 +55,9 @@ export class ModalSettings extends Modal {
       createAreaForNames();
     });
 
-    const wrapLanguage = document.createElement("div");
-    let languageType = this.localSettings.playerSettings.find(el => el.settingName === "language").settingValue;
+    const wrapLanguage = this.createTextBtn();
+    let languageType = this.findSetting("language");
     wrapLanguage.innerText = `${this.wordsArr[3]} ${languageType}`;
-    wrapLanguage.classList.add("modal__text-item");
     const nextLanguage = () => {
       const languages = ["English", "Russian", "Italian"];
       const currentLanguageIndex = languages.indexOf(languageType);
@@ -60,31 +70,26 @@ export class ModalSettings extends Modal {
       wrapLanguage.innerText = `${this.wordsArr[3]} ${languageType}`;
       this.localSettings.playerSettings.find(el => el.settingName === "language").settingValue = languageType;
     });
-    this.wrap.appendChild(wrapLanguage);
 
-    const wrapSound = document.createElement("div");
-    let soundType = this.localSettings.playerSettings.find(el => el.settingName === "sound").settingValue;
+    const wrapSound = this.createTextBtn();
+    let soundType = this.findSetting("sound");
     wrapSound.innerText = `${this.wordsArr[4]} ${soundType}`;
-    wrapSound.classList.add("modal__text-item");
     wrapSound.addEventListener("click", () => {
       this.checkPlaySound("A2");
       soundType = soundType === "on" ? "off" : "on";
       wrapSound.innerText = `${this.wordsArr[4]} ${soundType}`;
       this.localSettings.playerSettings.find(el => el.settingName === "sound").settingValue = soundType;
     });
-    this.wrap.appendChild(wrapSound);
 
-    const wrapColor = document.createElement("div");
-    let colorType = this.localSettings.playerSettings.find(el => el.settingName === "color").settingValue;
+    const wrapColor = this.createTextBtn();
+    let colorType = this.findSetting("color");
     wrapColor.innerText = `${this.wordsArr[5]} ${colorType}`;
-    wrapColor.classList.add("modal__text-item");
     wrapColor.addEventListener("click", () => {
       this.checkPlaySound("A2");
       colorType = colorType === "red" ? "green" : "red";
       wrapColor.innerText = `${this.wordsArr[5]} ${colorType}`;
       this.localSettings.playerSettings.find(el => el.settingName === "color").settingValue = colorType;
     });
-    this.wrap.appendChild(wrapColor);
 
     const saveSettingsBtn = this.createBtn(6);
     saveSettingsBtn.addEventListener("click", () => {
